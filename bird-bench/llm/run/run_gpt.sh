8158afb50d86ed87734c454743ae4035cc4310b8
Parthav Shergill <parthavshergill@Inflections-MacBook-Pro.local>
2025-07-01 08:03:17 -0700

    Add eval results for Qwen3 and XiYan-SQL models.

--- bird-bench/llm/run/run_gpt.sh
+++ bird-bench/llm/run/run_gpt.sh
@@ -5,7 +5,7 @@ use_knowledge='True'
 not_use_knowledge='False'
 mode='dev' # choose dev or dev
 cot='True'
-no_cot='Fales'
+no_cot='False'
 
 YOUR_API_KEY='iai-qN3iU0xE06L8M5EsZZqhRxXlxycKrOxaj7LE4JRkaWTyZuK9'
 
@@ -18,15 +18,15 @@ max_workers=5  # Number of parallel workers - adjust based on your API rate limi
 # data_kg_output_path='./exp_result/gpt_output_kg/'
 
 data_output_path='./exp_result/turbo_output/'
-data_kg_output_path='./exp_result/turbo_output_kg/'
+data_kg_output_path='./exp_result/qwen3_output_kg/'
 
 
-# echo 'generate GPT3.5 batch with knowledge'
-# python3 -u ./src/gpt_request.py --db_root_path ${db_root_path} --api_key ${YOUR_API_KEY} --mode ${mode} \
-# --engine ${engine3} --eval_path ${eval_path} --data_output_path ${data_kg_output_path} --use_knowledge ${use_knowledge} \
-# --chain_of_thought ${no_cot} --max_workers ${max_workers}
-
-echo 'generate GPT3.5 batch without knowledge'
+echo 'generate GPT3.5 batch with knowledge'
 python3 -u ./src/gpt_request.py --db_root_path ${db_root_path} --api_key ${YOUR_API_KEY} --mode ${mode} \
---engine ${engine3} --eval_path ${eval_path} --data_output_path ${data_output_path} --use_knowledge ${not_use_knowledge} \
---chain_of_thought ${no_cot} --max_workers ${max_workers}
+--engine ${engine3} --eval_path ${eval_path} --data_output_path ${data_kg_output_path} --use_knowledge ${use_knowledge} \
+--chain_of_thought ${cot} --max_workers ${max_workers}
+
+# echo 'generate GPT3.5 batch without knowledge'
+# python3 -u ./src/gpt_request.py --db_root_path ${db_root_path} --api_key ${YOUR_API_KEY} --mode ${mode} \
+# --engine ${engine3} --eval_path ${eval_path} --data_output_path ${data_output_path} --use_knowledge ${not_use_knowledge} \
+# --chain_of_thought ${no_cot} --max_workers ${max_workers}
